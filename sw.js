703cb09c23061ebd3c864e09c1ae88674e4371c7
Electrolux <3451613934@qq.com>
2023-01-30 18:43:53 +0800

    添加pwa的使用

--- sw.js
+++ sw.js
@@ -99,21 +99,4 @@ self.addEventListener('fetch', function(e) {
 // });
 
 
-var deferredPrompt = null;
-
-// 监听beforeinstallprompt事件，该事件在网站满足PWA安装条件时触发，保存安装事件
-window.addEventListener("beforeinstallprompt", e => {
-    e.preventDefault();
-    deferredPrompt = e;
-});
-
-// 监听appinstalled事件，该事件在用户同意安装后触发，清空安装事件
-window.addEventListener("appinstalled", () => {
-    deferredPrompt = null;
-});
-
-// 手动触发PWA安装
-function addToDesktop() {
-    deferredPrompt.prompt();
-}
 
