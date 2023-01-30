fddd819be50c1da1ceebf3afb02101a5a9709513
Electrolux <3451613934@qq.com>
2023-01-30 19:04:29 +0800

    添加pwa的使用

--- sw.js
+++ sw.js
@@ -39,23 +39,23 @@ self.addEventListener('install', function(e) {
 //重要：更新静态资源 可以遍历所有的缓存名称逐一判断决决定是否清除
 self.addEventListener('activate', function(e) {
   console.log('Activate event')
-  e.waitUntil(
-    Promise.all(
-      caches.keys().then(cacheNames => {
-        return cacheNames.map(name => {
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-        })
-      })
-    ).then(() => {
-      console.log('Clients claims.')
-      //在新安装的 Service Worker 中通过调用 self.clients.claim() 
-      //取得页面的控制权, 这样之后打开页面都会使用版本更新的缓存。
-      //旧的 Service Worker 脚本不再控制着页面之后会被停止
-      return self.clients.claim()
-    })
-  )
+  // e.waitUntil(
+  //   Promise.all(
+  //     caches.keys().then(cacheNames => {
+  //       return cacheNames.map(name => {
+  //         if (name !== cacheStorageKey) {
+  //           return caches.delete(name)
+  //         }
+  //       })
+  //     })
+  //   ).then(() => {
+  //     console.log('Clients claims.')
+  //     //在新安装的 Service Worker 中通过调用 self.clients.claim() 
+  //     //取得页面的控制权, 这样之后打开页面都会使用版本更新的缓存。
+  //     //旧的 Service Worker 脚本不再控制着页面之后会被停止
+  //     return self.clients.claim()
+  //   })
+  // )
 })
 
 //重要：处理动态缓存
